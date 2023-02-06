373eb3f007cbdb584d0b6cbde31a43aa923244ee
islam.t <islam.t@snoonu.com>
2023-02-06 20:16:21 +0600

    Initial Commit

---
+++ startios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  startios
+//
+//  Created by Islam Temirbek on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
